3ac5f86fad19b168769a947a742eda7dd820e003
alim4 <theineffablebob@gmail.com>
2014-04-21 01:29:46 -0700

    Added average per-period return, mean balance, normal variation stuff

--- RandomWalk.py
+++ RandomWalk.py
@@ -9,6 +9,7 @@
 __author__ = 'anthonylim'
 
 import random
+import math
 
 
 def main():
@@ -20,10 +21,22 @@ def main():
     avg_return, stddev_return, leverage_ratio, number_of_periods, number_of_trials = getInputs()
 
     # Run the simulation
-    simulate(avg_return, stddev_return, leverage_ratio, number_of_periods)
+    total_balance = 0
+    total_period_return = 0
+    for i in range(number_of_trials):
+        bal, preturn = simulate(avg_return, stddev_return, leverage_ratio, number_of_periods)
+        total_balance += bal
+        total_period_return += preturn
+
+    mean_balance = total_balance / number_of_trials
+    mean_period_return = total_period_return / number_of_trials
+    print('mean_balance: {0} | mean_return: {1}'.format(mean_balance, mean_period_return))
 
     print("We ran {0} trials, each at {1}% return ({2}% std dev) for {3} periods."
           .format(number_of_trials, avg_return, stddev_return, number_of_periods))
+
+    print("Average per-period return: {0:.4g}%".format(mean_period_return))
+    #print("Standard deviation of return: {0:.2g}%".format())
 
     print("avg_return: {0}\n"
           "stddev_return: {1}\n"
@@ -72,17 +85,37 @@ def getInputs():
             number_of_periods,
             number_of_trials)
 
+def calculateStandardDev(observed_value):
+    m1 = observed_value
+    s1 = 0.0
+
+
+
 
 def simulate(avg_return, stddev_return, leverage_ratio, number_of_periods):
     orig_balance = 100.0
+    curr_balance = orig_balance
     num_periods = number_of_periods
-    curr_balance = 0
+    num_broke = 0
     while num_periods > 0:
-        print('num_periods: {0}'.format(num_periods))
-        curr_balance += random.normalvariate(avg_return, stddev_return) * leverage_ratio * curr_balance
+        #print('num_periods: {0}'.format(num_periods))
+        curr_balance += random.normalvariate(avg_return, stddev_return) * leverage_ratio
+        if curr_balance < 0:
+            curr_balance = 0
+            num_broke += 1
         num_periods -= 1
 
-    return
+    ratio = curr_balance / orig_balance
+    per_period_return = (math.pow(float(ratio), 1/float(number_of_periods)) - 1) * 100
+
+    #print('number_of_periods: {0}'.format(number_of_periods))
+    #print('curr_bal: {0} | orig_bal: {1}'.format(curr_balance, orig_balance))
+    #print('ratio: {0} | ppr: {1}'.format(ratio, per_period_return))
+    #print('num times broke: {0}'.format(num_broke))
+
+    #print('balance: {0}'.format(curr_balance))
+    return (curr_balance,
+            per_period_return)
 
 
 main()
