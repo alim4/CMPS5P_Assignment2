3d1fc5016cf113fee473756e10e25f4678bbe60d
alim4 <theineffablebob@gmail.com>
2014-04-21 23:24:54 -0700

    Added percentage modifications, clean up of print messages

--- RandomWalk.py
+++ RandomWalk.py
@@ -15,7 +15,8 @@ import math
 
 def main():
     global unittest
-    unittest = 1
+    # Set to 1 to enable auto-input of test values
+    unittest = 0
     # Gets input from user and stores into variables
     avg_return, stddev_return, leverage_ratio, number_of_periods, number_of_trials = getInputs()
 
@@ -37,11 +38,10 @@ def main():
         total_num_broke += num_broke
         total_losses += ppr_losses
         stddev_list.append(preturn)
-        if ppr_losses != 0:
-            stddev_losses.append(ppr_losses)
+        stddev_losses.append(ppr_losses)
 
     # Standard deviation regular
-    stddev = calculateStandardDev(stddev_list)
+    stddev = calculateStandardDev(stddev_list) * 1000
 
     # Standard deviation losses
     stddev_loss = calculateStandardDev(stddev_losses)
@@ -51,13 +51,12 @@ def main():
     num_broke_percent = (total_num_broke / (number_of_periods * number_of_trials)) * 100
 
     mean_balance = float(total_balance / number_of_trials)
-    mean_period_return = (total_period_return / number_of_trials) * 10
+    mean_period_return = (total_period_return / number_of_trials) * 10000
     mean_ppr_losses = (total_losses / number_of_trials)
-    print('mean_balance: {0} | mean_return: {1}'.format(mean_balance, mean_period_return))
+    # print('mean_balance: {0} | mean_return: {1}'.format(mean_balance, mean_period_return))
 
     print("We ran {0} trials, each at {1:.4g}% return ({2:.4g}% std dev) for {3} periods. "
-          "({4} total runs)"
-          .format(number_of_trials, avg_return, stddev_return, number_of_periods, number_of_periods*number_of_trials))
+          .format(number_of_trials, avg_return*100, stddev_return*100, number_of_periods))
 
     print("Went broke {0} times ({1:.4g}% of the time)".format(total_num_broke, num_broke_percent))
     print("Average per-period return: {0:.4g}%".format(mean_period_return))
@@ -66,18 +65,15 @@ def main():
     print("For losses, average per-period return: {0:.4g}%".format(mean_ppr_losses))
     print("For losses, standard deviation of return: {0:.4g}%".format(stddev_loss))
 
-    print("\navg_return: {0}\n"
-          "stddev_return: {1}\n"
-          "leverage_ratio: {2}\n"
-          "number_of_periods: {3}\n"
-          "number_of_trials: {4}\n"
-          .format(avg_return, stddev_return, leverage_ratio, number_of_periods, number_of_trials))
+    # print("\navg_return: {0}\n"
+    #       "stddev_return: {1}\n"
+    #       "leverage_ratio: {2}\n"
+    #       "number_of_periods: {3}\n"
+    #       "number_of_trials: {4}\n"
+    #       .format(avg_return, stddev_return, leverage_ratio, number_of_periods, number_of_trials))
 
 def calculateStandardDev(stddev_list):
     # Standard deviation calculations
-    """
-    :rtype : float
-    """
     stddev_list_squared = 0.0
     for j in stddev_list:
         stddev_list_squared += math.pow(j, 2)
@@ -94,6 +90,7 @@ def getInputs():
         leverage_ratio = float(raw_input("Input leverage ratio (1.0-10.0): "))
         number_of_periods = int(raw_input("Input number of periods (10-500): "))
         number_of_trials = int(raw_input("Input number of trials (100-10000): "))
+        print("")
     elif unittest == 1:
         print("UNIT TEST MODE ACTIVE")
         avg_return = float(2.0)
@@ -129,6 +126,9 @@ def getInputs():
     elif number_of_trials < 100:
         number_of_trials = 100
 
+    avg_return /= 100
+    stddev_return /= 100
+
     return (avg_return,
             stddev_return,
             leverage_ratio,
@@ -144,14 +144,14 @@ def simulate(avg_return, stddev_return, leverage_ratio, number_of_periods):
     losses = 0.0
 
     while num_periods > 0:
-        #print('num_periods: {0}'.format(num_periods))
-        curr_balance += random.normalvariate(avg_return, stddev_return) * (leverage_ratio * orig_balance)
+        # print('num_periods: {0}'.format(num_periods))
+        curr_balance += random.normalvariate(avg_return, stddev_return) * leverage_ratio
         if curr_balance < orig_balance:
             if curr_balance < 0:
                 num_broke += 1
+                curr_balance = 0
             else:
                 losses += curr_balance
-            curr_balance = 0
             num_lost_money += 1
         num_periods -= 1
 
